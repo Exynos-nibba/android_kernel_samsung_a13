fcd029c0df95c80465bfd80618bc1d446392e06f
Exynos-nibba <vsht700@gmail.com>
2022-10-20 18:48:29 +0000

    Makefile: path update

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
+export CROSS_COMPILE=$(pwd)/../gas/bin/aarch64-linux-gnu-
+export CC=$(pwd)/../clang/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
+export PATH=$(pwd)/../clang/bin:$PATH
+export PATH=$(pwd)/../gas/bin:$PATH
 export ARCH=arm64
 export ANDROID_MAJOR_VERSION=r
 
