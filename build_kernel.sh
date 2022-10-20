cb80c294fbd0b7f8d9761ea66ce8369b3932aaaf
Exynos-nibba <vsht700@gmail.com>
2022-10-20 18:48:29 +0000

    chernobyl: bringup

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,6 +7,7 @@ export PATH=$(pwd)/../clang/bin:$PATH
 export PATH=$(pwd)/../gas/bin:$PATH
 export ARCH=arm64
 export ANDROID_MAJOR_VERSION=r
+export VARIANT="A13"
 
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
@@ -14,4 +15,12 @@ export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a13ve_defconfig
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+git clone https://github.com/osm0sis/AnyKernel3 $(pwd)/AnyKernel3
+cd AnyKernel3
+git reset --hard
+cp $(pwd)/../out/arch/arm64/boot/Image zImage
+rm -rf anykernel.sh
+cp ../ak3-script/anykernel.sh ./
+zip -r9 $(pwd)/../${VARIANT}_chernobylkernel_`date '+%Y_%m_%d'`.zip * -x .git README.md *placeholder
+
+#cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
