1e5402b1754fe791c844a8d17d95b7fc5fb89148
Exynos-nibba <vsht700@gmail.com>
2023-01-04 19:14:44 +0000

    chernobyl: bringup

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
+export CROSS_COMPILE=$(pwd)/../gas/bin/aarch64-linux-gnu-
+export CC=$(pwd)/../clang/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
+export PATH=$(pwd)/../clang/bin:$PATH
+export PATH=$(pwd)/../gas/bin:$PATH
 export ARCH=arm64
-
+export VARIANT="A13"
 
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
+make clean
+rm -rf out
+
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a13ve_defconfig
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+git clone https://github.com/osm0sis/AnyKernel3 $(pwd)/AnyKernel3
+cd AnyKernel3
+git reset --hard HEAD
+cp $(pwd)/../out/arch/arm64/boot/Image zImage
+rm -rf anykernel.sh
+cp ../ak3-script/anykernel.sh ./
+zip -r9 $(pwd)/../${VARIANT}_chernobylkernel_`date '+%Y_%m_%d'`.zip * -x .git README.md *placeholder
+
+#cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
