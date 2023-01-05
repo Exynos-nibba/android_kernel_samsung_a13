299abeee5bf44300dabfc9a39538bdcb175ea5a9
Exynos-nibba <vsht700@gmail.com>
2023-01-05 09:09:07 +0000

    chernobyl: version bringup

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# version numbering
+vers=$(cat versioning.txt)
+version=$(expr ${vers} + 1)
+rm -rf versioning.txt
+echo ${version} >> versioning.txt
+
+
 export CROSS_COMPILE=$(pwd)/../gas/bin/aarch64-linux-gnu-
 export CC=$(pwd)/../clang/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
@@ -7,7 +14,7 @@ export PATH=$(pwd)/../clang/bin:$PATH
 export PATH=$(pwd)/../gas/bin:$PATH
 export ARCH=arm64
 export VARIANT="A13"
-
+export LOCALVERSION=-chernobyl-V${vers}
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
@@ -23,6 +30,7 @@ git reset --hard HEAD
 cp $(pwd)/../out/arch/arm64/boot/Image zImage
 rm -rf anykernel.sh
 cp ../ak3-script/anykernel.sh ./
-zip -r9 $(pwd)/../${VARIANT}_chernobylkernel_`date '+%Y_%m_%d'`.zip * -x .git README.md *placeholder
+#zip -r9 $(pwd)/../${VARIANT}_chernobylkernel_$(date '+%Y_%m_%d').zip * -x .git README.md *placeholder
+zip -r9 $(pwd)/../${VARIANT}_chernobylkernel_${vers}.zip * -x .git README.md *placeholder
 
 #cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
